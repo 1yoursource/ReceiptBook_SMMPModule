be04a538b8f03ff523717dfa72221ad889c816c7
1yoursource <leo00pobeda@gmail.com>
2021-03-26 11:24:20 +0200

    z

---
+++ app/src/main/java/com/dnyana/recipebook/IndianStreetFoodFragment.java
@@ -0,0 +1,8 @@
+package com.dnyana.recipebook;
+
+public class IndianStreetFoodFragment extends CategorizedFragment {
+    @Override
+    protected int getFragmentLayout() {
+        return R.layout.fragment_indian_street_food;
+    }
+}
